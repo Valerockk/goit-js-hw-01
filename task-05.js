b173180eb58591340d82ab65569f9ec8347beca3
Valerii Troian <trojan.valerii@gmail.com>
2020-05-19 23:24:11 +0300

    Module 01

--- task-05.js
+++ task-05.js
@@ -1,5 +1,5 @@
 let country;
-let price;
+let price = 0;
 
 country = prompt('Введите пожалуйста страну от куда нужно доставить товар:');
 
@@ -8,29 +8,30 @@ country = country.toLowerCase();
 switch(country){
     case 'китай':
         price = 100;
-        console.log(`Доставка в ${country} будет стоить ${price} кредитов`);
         break;
 
     case 'чили':
         price = 250;
-        console.log(`Доставка в ${country} будет стоить ${price} кредитов`);
         break;
 
     case 'австралия':
         price = 170;
-        console.log(`Доставка в ${country} будет стоить ${price} кредитов`);
         break;
 
     case 'индия':
         price = 80;
-        console.log(`Доставка в ${country} будет стоить ${price} кредитов`);
         break;
 
     case 'ямайка':
         price = 120;
-        console.log(`Доставка в ${country} будет стоить ${price} кредитов`);
         break;
 
     default:
         alert('В вашей стране доставка недоступна!');
+}
+
+if(price <= 0){
+    console.log('В вашей стране доставка недоступна!');
+} else{
+    console.log(`Доставка в ${country} будет стоить ${price} кредитов`);
 }
